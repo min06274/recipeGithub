002222a06a711a7b0579c03ee498a3320376ea36
Eddy <min06274@naver.com>
2023-02-21 14:27:49 +0900

    use jetPack Navigation 바텀 네비게이션에서 메뉴 누르면 각각 프라그먼트로 이동

---
+++ app/src/main/java/min/bo/recipe/app/CartridgeFragment.kt
@@ -0,0 +1,18 @@
+package min.bo.recipe.app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CartridgeFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_cartridge,container,false)
+    }
+}
--- app/src/main/java/min/bo/recipe/app/MainActivity.kt
+++ app/src/main/java/min/bo/recipe/app/MainActivity.kt
@@ -2,6 +2,8 @@ package min.bo.recipe.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         bottomNavigationView.itemIconTintList = null
 
+
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let{
+            bottomNavigationView.setupWithNavController(it)
+        }
 
     }
 }
