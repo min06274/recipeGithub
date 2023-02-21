31e6112dff73539291d9b2136279a632a93e98a9
Eddy <min06274@naver.com>
2023-02-21 13:45:15 +0900

    splash & bottom navigation (+ icon)

--- app/src/main/java/min/bo/recipe/app/MainActivity.kt
+++ app/src/main/java/min/bo/recipe/app/MainActivity.kt
@@ -2,10 +2,17 @@ package min.bo.recipe.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
+
     }
 }
