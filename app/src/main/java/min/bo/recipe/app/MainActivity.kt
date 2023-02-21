930d8f9cb968ba92a611001951659109fd15bcb9
Eddy <min06274@naver.com>
2023-02-21 12:24:02 +0900

    프로젝트 생성

---
+++ app/src/main/java/min/bo/recipe/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package min.bo.recipe.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
