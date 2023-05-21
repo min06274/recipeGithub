5f6c67a737522bc9c7652abc29f2d998d9f799a8
Eddy <min06274@naver.com>
2023-05-21 17:38:26 +0900

    파베 뺌

--- app/src/main/java/min/bo/recipe/app/ui/cartridge/CartridgeFragment.kt
+++ app/src/main/java/min/bo/recipe/app/ui/cartridge/CartridgeFragment.kt
@@ -1,9 +1,10 @@
-package min.bo.recipe.app.ui
+package min.bo.recipe.app.ui.cartridge
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import min.bo.recipe.app.R
 
@@ -16,4 +17,22 @@ class CartridgeFragment:Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_cartridge,container,false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val jsonButton: Button = view.findViewById(R.id.json_btn)
+        
+        
+        jsonButton.setOnClickListener{
+            jsonButton.text = "이이이"
+        }
+
+
+
+
+
+    }
+
 }
